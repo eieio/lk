24cafcfcd0e064cb34aecd3bade701b3eaed36d2
Corey Tabaka <corey.tabaka@gmail.com>
2013-05-29 00:53:29 -0700

    [lib] Add libagg C++ low-level graphics library.

---
+++ lib/agg/rules.mk
@@ -0,0 +1,13 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+GLOBAL_INCLUDES += $(LOCAL_DIR)/include
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/agg_embedded_raster_fonts.cpp \
+	$(LOCAL_DIR)/agg_vcgen_stroke.cpp \
+	$(LOCAL_DIR)/agg_rounded_rect.cpp \
+	$(LOCAL_DIR)/agg_arc.cpp \
+
+include make/module.mk
