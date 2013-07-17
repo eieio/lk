c8ea7aba84877febfaaad40c3cc1893df214ad08
Corey Tabaka <corey.tabaka@gmail.com>
2013-07-17 00:42:08 -0700

    [app][sq] Remove network test commands from app/sq.

--- app/sq/main.c
+++ app/sq/main.c
@@ -113,9 +113,6 @@ static void sq_init(const struct app_descriptor *app)
 	sq_pop(sqvm, 1); //pops the root table
 }
 
-#include <lwip/api.h>
-#include <lwip/ip_addr.h>
-
 static void sq_entry(const struct app_descriptor *app, void *args)
 {
 	call_vm(sqvm, _SC("run"));
@@ -126,44 +123,3 @@ APP_START(sq)
 	.entry = sq_entry,
 APP_END
 
-#if defined(WITH_LIB_CONSOLE)
-#include <lib/console.h>
-
-static int net_cmd(int argc, const cmd_args *argv)
-{
-	if (argc < 2) {
-		printf("%s commands:\n", argv[0].str);
-usage:
-		printf("%s lookup <hostname>\n", argv[0].str);
-		goto out;
-	}
-
-	if (!strcmp(argv[1].str, "lookup")) {
-		if (argc < 3)
-			goto usage;
-
-		ip_addr_t ip_addr;
-		const char *hostname = argv[2].str;
-		err_t err;
-
-		err = netconn_gethostbyname(hostname, &ip_addr);
-		if (err != ERR_OK) {
-			printf("Failed to resolve host: %d\n", err);
-		} else {
-			printf("%s: %u.%u.%u.%u\n", hostname,
-					ip4_addr1_16(&ip_addr),
-					ip4_addr2_16(&ip_addr),
-					ip4_addr3_16(&ip_addr),
-					ip4_addr4_16(&ip_addr));
-		}
-	}
-
-out:
-	return 0;
-}
-
-STATIC_COMMAND_START
-{ "net", "net toolbox", &net_cmd },
-STATIC_COMMAND_END(net);
-
-#endif
